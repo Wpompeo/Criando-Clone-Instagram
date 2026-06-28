1e82f27a823949718a45b805e97e049c0dac8e88
Wagner Pompeo <wag_pompeo@yahoo.com.br>
2026-06-28 13:07:17 -0300

    First commit project

---
+++ app/src/main/java/com/wts/socialnetwork/login/view/login/view/LoginActivity.kt
@@ -0,0 +1,53 @@
+package com.wts.socialnetwork.login.view.login.view
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.text.Editable
+import android.text.TextWatcher
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+import com.wts.socialnetwork.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val editTextEmail = findViewById<TextInputEditText>(R.id.login_edit_email)
+        val editTextPassword = findViewById<TextInputEditText>(R.id.login_edit_password)
+
+        editTextEmail.addTextChangedListener(watcher)
+        editTextPassword.addTextChangedListener(watcher)
+
+        val buttonEnter = findViewById<LoadingButton>(R.id.login_btn_enter)
+        buttonEnter.setOnClickListener {
+            buttonEnter.showProgress(true)
+
+            findViewById<TextInputLayout>(R.id.login_edit_email_input)
+                .error = "E-mail inválido"
+
+            findViewById<TextInputLayout>(R.id.login_edit_password_input)
+                .error = "Senha incorreta"
+
+            Handler(Looper.getMainLooper()).postDelayed({
+                buttonEnter.showProgress(false)
+
+            }, 500)
+        }
+    }
+
+    private val watcher = object : TextWatcher {
+        override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+        }
+
+        override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            findViewById<LoadingButton>(R.id.login_btn_enter).isEnabled = p0.toString().isNotEmpty()
+        }
+
+        override fun afterTextChanged(p0: Editable?) {
+
+        }
+    }
+}
