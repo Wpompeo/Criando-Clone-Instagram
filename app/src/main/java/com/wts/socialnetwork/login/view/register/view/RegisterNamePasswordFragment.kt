ae429189a07aaa6ad80e43e5402b2102b498cacc
Wagner Pompeo <wag_pompeo@yahoo.com.br>
2026-06-29 09:45:59 -0300

    Three commit, development new files and fragments

--- app/src/main/java/com/wts/socialnetwork/login/view/register/view/RegisterNamePasswordFragment.kt
+++ app/src/main/java/com/wts/socialnetwork/login/view/register/view/RegisterNamePasswordFragment.kt
@@ -1,4 +1,19 @@
 package com.wts.socialnetwork.login.view.register.view
 
-class RegisterNamePasswordFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.wts.socialnetwork.R
+import androidx.fragment.app.Fragment
+
+class RegisterNamePasswordFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_register_name_password, container, false)
+    }
 }
