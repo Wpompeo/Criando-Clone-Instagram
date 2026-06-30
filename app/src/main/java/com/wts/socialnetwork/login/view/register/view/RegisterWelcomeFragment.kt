bdcd8d5fa9ce31ec558f855fbe5bd633e887a569
Wagner Pompeo <wag_pompeo@yahoo.com.br>
2026-06-29 23:39:38 -0300

    New files: activity_register.xml, fragment_register_welcome.xml and development em styles.xml file

---
+++ app/src/main/java/com/wts/socialnetwork/login/view/register/view/RegisterWelcomeFragment.kt
@@ -0,0 +1,15 @@
+package com.wts.socialnetwork.login.view.register.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.wts.socialnetwork.R
+import androidx.fragment.app.Fragment
+
+class RegisterWelcomeFragment : Fragment(){
+
+    override fun onCreateView( inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_register_welcome, container, false )
+    }
+}
